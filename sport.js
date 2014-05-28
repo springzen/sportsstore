f8652bbdda723d062bc3264427a42a65b8505eb9
wlehman <wwlehman@gmail.com>
2014-05-28 09:58:21 -0700

    add node starter

--- sport.js
+++ sport.js
@@ -1,5 +1,17 @@
+/**
+ * get connect dependency
+ * @type {exports}
+ */
 var connect = require('connect');
+
+/**
+ * Dummy message for terminal to show we are starting
+ */
 console.log("Connecting...");
+
+/**
+ * start the server and connect to the given port
+ */
 connect.createServer(
-	connect.static("./sportsstore")
+    connect.static("./sportsstore")
 ).listen(5001);
